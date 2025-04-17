3902eb2a2fce83db3f51e7d3ff53c34a658d6daa
jmpizza <jespitiap@unal.edu.co>
2025-04-17 14:26:14 -0500

    configuracion de movimiento basico en el personaje

---
+++ scenes/player/player.gd
@@ -0,0 +1,13 @@
+class_name Player
+extends Node2D
+
+@export var speed = 400
+
+func _physics_process(delta: float) -> void:
+	get_input()
+	move_and_slide()
+	
+func get_input():
+	var input_direction =  Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction*speed
+	
